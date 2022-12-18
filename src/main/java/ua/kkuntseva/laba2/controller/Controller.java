5b72fc7d519cabc6d555b88c83c337c38b46b7ce
KuntsevaKateryna <Milka602602>
2022-12-18 17:15:25 +0200

    factory framework was realized to organize the writing articles into ms word

--- src/main/java/ua/kkuntseva/laba2/controller/Controller.java
+++ src/main/java/ua/kkuntseva/laba2/controller/Controller.java
@@ -1,5 +1,6 @@
 package ua.kkuntseva.laba2.controller;
 
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.thymeleaf.util.StringUtils;
+import org.thymeleaf.util.TextUtils;
+import ua.kkuntseva.laba2.factory.FormatGenerator;
+import ua.kkuntseva.laba2.factory.MyFactory;
 import ua.kkuntseva.laba2.model.Article;
 import ua.kkuntseva.laba2.service.NewsLoader;
 import ua.kkuntseva.laba2.service.NewsParser;
 import ua.kkuntseva.laba2.service.NewsParserImpl;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.StringJoiner;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.regex.Pattern;
@@ -50,22 +57,21 @@ public class Controller {
     }
 
     @PostMapping("/home")
-    public String searchFilm(ModelMap model,
-                             // @RequestParam String q,
-                             @RequestParam String from,
-                             @RequestParam String to,
-                             @RequestParam(name = "category", required = false, defaultValue = "") String category,
-                             @RequestParam String country) {
+    public String searchArticle(ModelMap model,
+                                // @RequestParam String q,
+                                @RequestParam String from,
+                                @RequestParam String to,
+                                @RequestParam(name = "category", required = false, defaultValue = "") String category,
+                                @RequestParam String country) {
         String rez = null;
         logger.info("start to send request");
         long startTime = System.currentTimeMillis();
         try {
-
             String[] category_array = category.split(Pattern.quote(","));
             System.out.println("category: " + category);
             for (int i = 0; i < category_array.length; i++) {
                 System.out.println("category_array[i]: " + category_array[i]);
-                CompletableFuture<String> result = //async_findArticle
+                CompletableFuture<String> result =
                         newsLoader.findArticle
                                 (site_address,
                                         apikey_value,
@@ -74,27 +80,38 @@ public class Controller {
                                         to,
                                         category_array[i],
                                         country);
+
                 result.thenAcceptAsync(ii -> {
                     logger.warn("finished name result = " + result);
                 });
                 //articles_info - a collection of received info about articles, shown in textarea
-               articles_info.add(result.get());
+                articles_info.add(result.get());
                 rez = String.join("\n\n", articles_info);
-               model.addAttribute("description", rez);
-              // show_result_in_textarea(result, rez, model);
+                model.addAttribute("description", rez);
 
                 //articles - a collection of Articles, ready to be written to Word document
-                articles.add(newsParser.parseJSON(result.get()));
+                articles = newsParser.parseJSON(result.get());
+                System.out.println("++result.get() :" + result.get());
             }
             long endTime = System.currentTimeMillis();
             System.out.println("Common Process time :" + (endTime - startTime));
         } catch (InterruptedException e) {
-            logger.error(e.getMessage());
+            logger.info("InterruptedException " + e.getMessage());
         } catch (ExecutionException e) {
             logger.error(e.getMessage());
         } catch (Exception e) {
             logger.error(e.getMessage());
         }
+        return "index";
+    }
+
+    @PostMapping("/save")
+    public String saveInfo() {
+        MyFactory f = new MyFactory();
+        FormatGenerator msWordStructure = f.create_format("doc");
+        System.out.println("this.articles.size() :" + articles.size());
+        XWPFDocument doc1 = (XWPFDocument) msWordStructure.generateDocumentStructure(articles);
+        msWordStructure.saveFile(doc1, "1.docx");
         return "index";
     }
 
